d9927b8acb36609a8339340117da3c8986a4a61b
Sergio45230 <paolantonio.sergio@gmail.com>
2023-01-13 08:26:17 -0300

    Actualizacion de Visual Studio Code

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 'use strict';
 var http = require('http');
-var port = process.env.PORT || 3000;
+const PORT = 3000;
+
+const server = app.listen(PORT, () =>
+	console.log(`Server started on port http://localhost:${PORT}`)
+);
+server.on("error", (err) => console.log(err));
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
-}).listen(port);
+}).listen(PORT);
+
